9587f509e66ee8af40fd713224ca75a7c4843e3a
rlnp1 <raulnapalla21@gmail.com>
2022-09-23 10:01:47 +0800

    updated json server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todolist';
+  constructor(private http: HttpClient){
+    http.get("http://localhost:3000/posts").subscribe((output)=>{
+      console.log(output);
+      
+    })
+  }
 }
